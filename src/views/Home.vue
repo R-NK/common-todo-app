a904da5300a1efc7eb9c75f4861d9feec9a668cd
R-NK <right.rcon.f@outlook.com>
2018-11-10 13:18:09 +0900

    you can save todos

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,14 +5,14 @@
       <v-container grid-list-md fluid>
         <v-layout row wrap>
           <v-flex md3 v-for="(item, index) in todoItems" :key="item.id">
-            <card :item="item" @remove="todoItems.splice(index, 1)" @editItem="editItem"></card>
+            <card :item="item" @remove="removeItem(index)" @editItem="editItem"></card>
           </v-flex>
           <v-flex md3>
             <card-add @addItem="addItem"></card-add>
           </v-flex>
         </v-layout>
       </v-container>
-      <modal-card @change-item="changeItem" ref="dialogue"></modal-card>
+      <modal-card @change-item="saveItem" ref="dialogue"></modal-card>
     </v-container>
   </v-content>
 </template>
@@ -34,10 +34,31 @@ import { DateTime } from 'luxon';
 })
 export default class Home extends Vue {
   public todoItems: TodoItem[] = [
-    new TodoItem(1, 'todo1', 'ひとつめのtodo', '1995-12-17T03:24:00'),
-    new TodoItem(2, 'todo2', 'ふたつめのtodo', '2105-04-09T23:24:00'),
-    new TodoItem(3, 'todo3', 'みっつめのtodo', '3201-08-23T01:24:00'),
+    {id: 1, title: 'todo1', description: 'ひとつめのtodo', datetime: '1995-12-17T03:24:00'},
+    {id: 2, title: 'todo2', description: 'ふたつめのtodo', datetime: '2105-04-09T23:24:00'},
+    {id: 3, title: 'todo3', description: 'みっつめのtodo', datetime: '3201-08-23T01:24:00'},
   ];
+
+  public mounted() {
+    const todoItems = this.getTodoItems();
+    if (todoItems != null) {
+      this.todoItems = todoItems;
+    } else {
+      console.log('something went wrong...');
+    }
+  }
+
+  public getTodoItems(): TodoItem[] {
+    const todoData = localStorage.getItem('todoItems');
+    const todoItems: TodoItem[] = [];
+    if (todoData != null) {
+      const todoJson = JSON.parse(todoData) as TodoItem[];
+      for (const item of todoJson) {
+        todoItems.push({id: item.id, title: item.title, description: item.description, datetime: item.datetime});
+      }
+    }
+    return todoItems;
+  }
 
   public editItem(item: TodoItem): void {
     console.log(JSON.stringify(item));
@@ -45,7 +66,7 @@ export default class Home extends Vue {
     dialogue.open(item);
   }
 
-  public changeItem(newItem: TodoItem): void {
+  public saveItem(newItem: TodoItem): void {
     // add new item
     if (!this.todoItems.some((element) =>  element.id === newItem.id)) {
       this.todoItems.push(newItem);
@@ -57,17 +78,23 @@ export default class Home extends Vue {
         return element;
       }
     });
+    localStorage.setItem('todoItems', JSON.stringify(this.todoItems));
     console.log(JSON.stringify(this.todoItems));
   }
 
   public addItem(): void {
     const currentUTC = DateTime.local().toUTC().toISO();
-    const newItem = new TodoItem(this.getNewId(this.todoItems), '', '', currentUTC);
+    const newItem = {id: this.getNewId(this.todoItems), title: '', description: '', datetime: currentUTC};
     const dialogue: any = this.$refs.dialogue;
     dialogue.open(newItem);
   }
 
-   private getNewId(items: TodoItem[]): number {
+  public removeItem(index: number): void {
+    this.todoItems.splice(index, 1);
+    localStorage.setItem('todoItems', JSON.stringify(this.todoItems));
+  }
+
+  private getNewId(items: TodoItem[]): number {
     const ids = items.map((element) => element.id);
     return Math.max(...ids) + 1;
   }
